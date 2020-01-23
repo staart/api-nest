4f4fb1cba59c33a6abb713e00472d9da29130b2c
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-24 01:09:17 +0530

    :construction: Add MySQL database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,32 @@ import { AppService } from "./app.service";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
+import { User } from "./user/user.entity";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "static"),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || "root",
+      password: process.env.DB_PASSWORD || "root",
+      database: process.env.DB_DATABASE || "test",
+      entities: [User],
+      synchronize: true
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "static")
+    })
   ],
   controllers: [AppController],
   providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}

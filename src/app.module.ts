09641d9f643de06804f1ce35615cacb7a43925d1
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-24 00:57:00 +0530

    :sparkles: Add support for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "static"),
     }),

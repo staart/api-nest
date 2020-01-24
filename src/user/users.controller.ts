79d4393ba5168e044d812f1191dd9bce97dfabe3
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-24 14:48:36 +0530

    :wrench: Add API tags

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -9,6 +9,7 @@ import {
 } from "@nestjsx/crud";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Crud({
   model: {
@@ -18,6 +19,7 @@ import { UsersService } from "./users.service";
     exclude: ["password", "twoFactorSecret"]
   }
 })
+@ApiTags("users")
 @Controller("users")
 export class UsersController {
   constructor(public service: UsersService) {}

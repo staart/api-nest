b72346bd886c7958aa588e309c1d1d80bd0f62e6
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-25 00:35:10 +0530

    :wrench: Add access token expiry config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { jwtConstants } from "./constants";
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: "60s" }
+      signOptions: { expiresIn: jwtConstants.accessTokenExpiry }
     })
   ],
   providers: [AuthService, LocalStrategy],
--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,4 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET || "secretKey"
+  secret: process.env.JWT_SECRET || "secretKey",
+  accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY || "15m"
 };
